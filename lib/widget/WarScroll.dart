ef8da4adf7c401797ac18336992effbf700d0bc2
Jenny <jennyunger@t-online.de>
2026-05-08 01:43:52 +0200

    Bearbeiten der Seite WarScroll.dart, hinzufügen der groben Struktur einer Schriftrolle. Layout mit Rows und Columns muss nochmal überdacht werden zum import einer Waffe als Row.

--- lib/widget/WarScroll.dart
+++ lib/widget/WarScroll.dart
@@ -9,9 +9,11 @@ class WarScroll extends StatefulWidget {
 
   Settings settings;
   String title;
+
   //List<bool> erledigtList = [];
   //bool ownPhase = true;
   Color phaseColor = Colors.yellow.shade800;
+
   //String phaseColorString = "White";
 
   @override
@@ -33,7 +35,8 @@ class _WarScroll extends State<WarScroll> {
         centerTitle: true,
         backgroundColor: widget.phaseColor,
         actions: <Widget>[
-          ElevatedButton( // Menü Button
+          ElevatedButton(
+            // Menü Button
             style: ElevatedButton.styleFrom(
               foregroundColor: Colors.red,
               backgroundColor: Colors.blueGrey.shade800,
@@ -46,10 +49,178 @@ class _WarScroll extends State<WarScroll> {
           ),
         ],
       ),
-      body: CustomScrollView(
-        primary: false,
-        slivers: <Widget>[
-        ],
+      body: Container(
+        //transform: Matrix4.rotationZ(0.1),
+        child: Column(
+          //1. Column für 4 Rows
+          children: [
+            //1. Row für Kreis und Name der Einheit
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Move + [value]"),
+                    Text("Save + [value]"),
+                    Text("Control + [value]"),
+                    Text("Health + [value]"),
+                  ],
+                ),
+                SizedBox(width: 600),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Held der Armee",
+                      style: TextStyle(height: 2.5, fontSize: 25),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            //2. Row für Fernkampf-Waffen
+            Row(
+              children: [
+                Column(
+                    children: [
+                      Text("Icon")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Shooting Weapons")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Rng"),
+                      Text("1")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Att"),
+                      Text("5")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Hit"),
+                      Text("3+")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Wnd"),
+                      Text("2+")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Rnd"),
+                      Text("1")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Dmg"),
+                      Text("3")
+                    ]
+                ),
+              ],
+            ),
+
+            SizedBox(height: 10),
+
+            //3. Row für Nahkampf-Waffen
+            Row(
+              children: [
+                Column(
+                    children: [
+                      Text("Icon")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Melee Weapons")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Rng"),
+                      Text("1")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Att"),
+                      Text("5")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Hit"),
+                      Text("3+")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Wnd"),
+                      Text("2+")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Rnd"),
+                      Text("1")
+                    ]
+                ),
+                SizedBox(width: 5),
+                Column(
+                    children: [
+                      Text("Dmg"),
+                      Text("3")
+                    ]
+                ),
+              ],
+            ),
+
+            SizedBox(height: 30),
+
+            //4. Row für Schlüsselworte
+            Row(children: [
+              Column(
+                children: [
+                  Text("Keywords")
+                ],
+              ),
+              SizedBox(width: 30),
+              Column(
+                children: [
+                  Text("Hero, Cavalry"),
+                  Text("Great Alliance, Faction"),
+                ],
+              ),
+            ]
+            ),
+          ],
+        ),
       ),
     );
   }
