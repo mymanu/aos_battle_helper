f88d5c0ae890e91b19b54dbfa77447688ed52799
Manuel Dennig <manueldennig@web.de>
2026-06-06 13:27:21 +0200

    Units werden auf Charge und Move Seite angezeigt

--- lib/widget/MovementPhase.dart
+++ lib/widget/MovementPhase.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:aos_battle_helper/classes/battleFormation.dart';
 import 'package:flutter/material.dart';
 
@@ -30,6 +32,7 @@ class _MovementPhase extends State<MovementPhase> {
     String title = widget.title;
 
     List<Ability> spellsThisPhase = [];
+    List<Unit> unitsThisPhase = [];
 
     Functions functions = Functions();
 
@@ -80,6 +83,13 @@ class _MovementPhase extends State<MovementPhase> {
         widget.settings.ownPhase,
       ),
     );
+
+    //Ab hier Units in unitsThisPhase
+    for (Unit unit in widget.settings.army.unitList) {
+      if (!unit.move.contains("-")) {
+        unitsThisPhase.add(unit);
+      }
+    }
 
     //TODO ab Hier wird das UI der Helden-Phase gebaut, ab hier kann sich Jenny austoben.
     return Scaffold(
@@ -341,6 +351,275 @@ class _MovementPhase extends State<MovementPhase> {
                   ),
                 );
               }, childCount: spellsThisPhase.length),
+            ),
+          ),
+
+          //Ab hier Kacheln für Units in der unitsThisPhase List bauen
+          SliverPadding(
+            padding: const EdgeInsets.only(right: 50),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                /*
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                childAspectRatio: 1.1,
+                */
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                childAspectRatio: 2,
+              ),
+              delegate: SliverChildBuilderDelegate((
+                  BuildContext context,
+                  int index,
+                  ) {
+                return SingleChildScrollView(
+                  child: Card(
+                    child: unitsThisPhase[index].erledigt
+                        ? ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          unitsThisPhase[index].erledigt = false;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            unitsThisPhase[index].name,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Icon(Icons.done, size: 100),
+                          /*
+                              Container(
+                                height: 100,
+                                width: 100,
+                                child: LayoutBuilder(
+                                  builder: (context, constraint) {
+                                    return new Icon(
+                                      Icons.done,
+                                      size: constraint.biggest.height,
+                                    );
+                                  },
+                                ),
+                              ),
+                              */
+                        ],
+                      ),
+                    )
+                        : Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              unitsThisPhase[index].name,
+                              style: TextStyle(
+                                backgroundColor: widget.phaseColor,
+                                fontWeight: FontWeight.bold,
+                                color: calculateTextColor(
+                                  widget.phaseColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 10),
+                          child:
+                          // Idee Kreis getrennt durch 2 Linien
+                          Container(
+                            width: 200,
+                            height: 200,
+                            alignment: Alignment.centerLeft,
+                            decoration: BoxDecoration(
+                              color: Colors.white, // Circle color
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: widget.phaseColor,
+                                width: 7,
+                              ), // Optional border
+                            ),
+                            child: Stack(
+                              children: [
+                                // left top to right bottom
+                                Transform.rotate(
+                                  angle:
+                                  math.pi /
+                                      4, // 45 degrees in radians
+                                  child: Center(
+                                    child: Container(
+                                      height: 5, // Thickness
+                                      color: widget.phaseColor,
+                                    ),
+                                  ),
+                                ),
+                                Transform.rotate(
+                                  angle: math.pi / 4,
+                                  // right top to left bottom
+                                  child: Center(
+                                    child: Container(
+                                      width: 5, // Thickness
+                                      color: widget.phaseColor,
+                                    ),
+                                  ),
+                                ),
+                                //https://pub.dev/packages/flutter_custom_clippers
+                                Positioned(
+                                  top: 55,
+                                  left: 112,
+                                  child: Container(
+                                    height: 75,
+                                    width: 75,
+                                    decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                ),
+
+                                Positioned(
+                                  left: 75,
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        "Move",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        unitsThisPhase[index].move,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 80,
+                                  left: -10,
+                                  child: Row(
+                                    children: [
+                                      Transform.rotate(
+                                        angle: math.pi * 1.5,
+                                        child: Text(
+                                          "Health",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        unitsThisPhase[index].health,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 80,
+                                  left: 130,
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        unitsThisPhase[index].save,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Transform.rotate(
+                                        angle: math.pi * 0.5,
+                                        child: Text(
+                                          "Save",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 135,
+                                  left: 70,
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        unitsThisPhase[index]
+                                            .banishment
+                                            .contains("-")
+                                            ? unitsThisPhase[index]
+                                            .control
+                                            : unitsThisPhase[index]
+                                            .banishment,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        unitsThisPhase[index]
+                                            .banishment
+                                            .contains("-")
+                                            ? "Control"
+                                            : "Banish",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            TextButton(
+                              child: Text("Unit erledigt"),
+                              onPressed: () {
+                                setState(() {
+                                  unitsThisPhase[index].erledigt = true;
+                                });
+                              },
+                            ),
+                            const SizedBox(width: 8),
+                            TextButton(
+                              child: Text("show Warscroll"),
+                              onPressed: () {
+                                functions.showWarscroll(
+                                  context,
+                                  widget.settings,
+                                  unitsThisPhase[index],
+                                );
+                              },
+                            ),
+                            const SizedBox(width: 8),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }, childCount: unitsThisPhase.length),
             ),
           ),
         ],
